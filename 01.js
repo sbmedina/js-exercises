478e7144bb31fdf4cb8afd80f42a20887b9b84b1
Blas Medina <blasmedina5@gmail.com>
2021-03-27 13:34:38 -0300

    Ex01

---
+++ 01.js
@@ -0,0 +1,17 @@
+// 1) Variables y Operadores
+// a) Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+var num01 = 2;
+var num02 = 3;
+var total = num01 + num02
+console.log (total)
+// b) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+var name = 'Mark '
+var lastName = 'Twain'
+var fullName = name + lastName
+console.log (fullName)
+// c) Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado la suma una 3er variable (utilizar length).
+var day = 'sunday '
+var month = 'March'
+var date = day + month
+date.length
+console.log (date.length)
